b33c803ed45f9b6db6ce5424757ae6ec49882380
ml lombry <webmaster@resinence.fr>
2018-05-22 15:35:12 +0200

    add a method for class Form, this method called Text return a field textarea

--- class/form.php
+++ class/form.php
@@ -11,8 +11,12 @@ class Form{
 	 * @param  [type] $id    [field id]
 	 * @return [string]        [field html]
 	 */
-	public static function input($name,$type='text',$class=null,$id=null){
-		return '<input id="'.$id.'" class="'.$class.'" type="'.$type.'" name="'.$name.'">';
+	public static function Input($name,$type='text',$class=null,$id=null,$placeholder=null){
+		return '<input id="'.$id.'" class="'.$class.'" type="'.$type.'" name="'.$name.'" placeholder="'.$placeholder.'">';
+	}
+
+	public static function Text($name='textarea',$rows='10',$cols='50',$id=null,$class=null,$placeholder=null){
+		return "<textarea id=$id class=$class name=$name rows=$rows cols=$cols placeholder=$placeholder ></textarea>";
 	}
 		
 }
