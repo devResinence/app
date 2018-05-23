0733acc80aba477e71b7d9bbf9b81b9240896b1d
ml lombry <webmaster@resinence.fr>
2018-05-23 09:02:35 +0200

    i just added a new function folder why passed argument for auto generate autoloading while traveling through the class folder

--- class/Autoloaders.php
+++ class/Autoloaders.php
@@ -1,5 +1,8 @@
 <?php
+namespace app;
 class Autoloaders {
+	public static $folder ='class';
+	public static $arg=[];
 	/**
 	 * class autoload
 	 * @param array $arr if argument is not array so require name of all class else just this class
@@ -7,10 +10,33 @@ class Autoloaders {
 	public static function Autoloding($arr=[]){
 		if(is_array($arr)){
 			foreach($arr as $k =>$v){
-				require 'class/'.$v.'.php';
+				require 'class/'.$v;
 			}
 		}else{
 		 require 'class/'.$arr.'.php';
 		}
 	}
+
+	/**
+	 * search all file in a folder for autoloading class in this folder
+	 * @param  string $folder link of folder research
+	 * @return function         return autoloding function 
+	 */
+	public static function folders($folder='.'){
+		// open folder class for browse all file //
+		if($folder = opendir('./class')){
+			//as long as I'm fine, I scan the files //
+			while(false !==($file = readdir($folder))){
+				//if file don't .,..,index.php,autoloaders.php//
+				if($file !='.' && $file !='..' && $file !='index.php' && $file !='Autoloaders.php'){
+					//push in arg variable in this class //
+					array_push(self::$arg,$file);
+				}
+			}
+			// called autoloding with all file in folder //
+			self::Autoloding(self::$arg);
+		}
+
+	}
+
 }
--- index.php
+++ index.php
@@ -3,8 +3,8 @@ echo 'index.php';
 require 'vendor/autoload.php';
 //require_once('class/form.php');
 require 'class/Autoloaders.php';
-Autoloaders::Autoloding(['form','bootstrapForm']);
-
+//Autoloaders::Autoloding(['form','bootstrapForm']);
+var_dump(app\Autoloaders::folders());
 echo app\Form::Input('nom','checkbox');
 echo app\Form::Text();
 echo app\Form::Select(['name','fruit']);
