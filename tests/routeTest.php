4eb8430643e6fcbe8f16b1300b17a14c35d5101f
ml lombry <webmaster@resinence.fr>
2018-05-24 11:47:53 +0200

    add other assert

--- tests/routeTest.php
+++ tests/routeTest.php
@@ -4,9 +4,9 @@ require '../../class/route.php';
 class RouteTest extends TestCase{
 	
 	public static function testGet(){
-		self::assertEmpty(Route::get());
+		self::assertEmpty(Route::get('article'));
 		self::assertEquals([],Route::get());
-		
+		self::assertContains('article',Route::get('article'));
 	}
 	public static function testPost(){
 		if(empty(Route::post())){
