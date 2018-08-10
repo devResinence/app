63b7cea496c96d9fd1f1243a1e3a10c6006fb5b2
ml lombry <webmaster@resinence.fr>
2018-08-10 11:56:32 +0200

    index modified

--- index.php
+++ index.php
@@ -17,4 +17,4 @@ var_dump(Database::getInstance($config)->slct('*','article'));
 $get = Route::get();
 //echo app\HTML::Video('480','480','','https://archive.org/download/WebmVp8Vorbis/webmvp8.gif','https://archive.org/download/WebmVp8Vorbis/webmvp8_512kb.mp4','video/mp4');
 //echo app\HTML::Audio('controls','toto.wav','audio/wav');
-?>
+$t ='';
